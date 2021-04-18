326e6e7a5afea5c226badfa82be2cc3c3e1ad97c
juanDev-47 <juanp.arenas@udea.edu.co>
2021-04-18 17:00:54 -0500

    update

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,7 +6,7 @@ const router = Router();
 const fetch = require('node-fetch');
 
 router.get('/', async (req, res) =>{
-     const response = await fetch('https://jsonplaceholder.typicode.com/photos'); // recibimos la respuesta desde la api  https://jsonplaceholder.typicode.com/todos/1
+     const response = await fetch('https://jsonplaceholder.typicode.com/todos'); // recibimos la respuesta desde la api  https://jsonplaceholder.typicode.com/todos/1
      const users = await response.json(); // convertimos la peticion en formato json para poder procesarlo
      res.json(users);
 });
